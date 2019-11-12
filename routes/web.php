19c7494d356d017cb2ca4009f2a6b00460004beb
Eden Fils <ednfils@gmail.com>
2019-11-12 18:32:21 -0400

    setting up routes

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
+
+Route::get('/', 'PageController@index');
+
+
+/***** ADMIN ROUTES *****/
+
+
+Route::get('/admin/products', 'Admin\ProductController@index');
+Route::post('/admin/products', 'Admin\ProductController@store');
+Route::get('/admin/products/create', 'Admin\ProductController@create');
+Route::get('/admin/products/{id}', 'Admin\ProductController@show');
+Route::get('/admin/products/{id}/edit', 'Admin\ProductController@edit');
+Route::put('/admin/products/{id}/edit', 'Admin\ProductController@update');
+Route::delete('/admin/products/{id}/delete', 'Admin\ProductController@destroy');
+
+
